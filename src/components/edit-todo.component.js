feb0828cd9a8ee243b9ef0e722a1f6ca2e90b194
Katie Frances <katiebeeisdead@gmail.com>
2019-02-25 11:54:23 +1100

    Added form

---
+++ src/components/edit-todo.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class EditToDo extends Component {
+    render() {
+        return (
+            <React.Fragment>
+                <p>Welcome to the Edit Component</p>
+            </React.Fragment>
+        )
+    }
+}
---
+++ src/components/todos-list.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class TodosList extends Component {
+    render() {
+        return (
+            <React.Fragment>
+                <p>Welome to the Todos List Component!</p>
+            </React.Fragment>
+        )
+    }
+}
